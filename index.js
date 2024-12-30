1f695f48b55acbc615ed85b1269090c053cd64c9
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-12-30 16:26:54 +0600

    add vercel json

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
+        // await client.connect();
+        // await client.db("admin").command({ ping: 1 });
         console.log("☘️  You successfully connected to MongoDB!");
 
         // Database Collection
