87a3b34ae720c2bb968d43d1cd54cdeb03361b29
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-12-30 16:18:13 +0600

    setup full project

---
+++ index.js
@@ -0,0 +1,55 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@tariqul-islam.mchvj.mongodb.net/?retryWrites=true&w=majority&appName=TARIQUL-ISLAM`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("☘️  You successfully connected to MongoDB!");
+
+        // Database Collection
+        const menuCollection = client.db("Bistro_Boss").collection("Menu")
+        const reviewCollection = client.db("Bistro_Boss").collection("Reviews")
+
+        app.get('/menus', async (req, res) => {
+            const result = await menuCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray()
+            res.send(result)
+        })
+
+    } catch (error) {
+        console.log("MongoDB:", error.message);
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello Programmer. How Are You? This Server For Bistro Boss Restaurant Website ❤️')
+})
+
+app.listen(port, () => {
+    console.log(`☘️  You successfully connected to Server: ${port}`);
+})
